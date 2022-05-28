d062e43ec99a91addb9a4de179651ce992bba381
Andrea Sarmiento <anbesamo@gmail.com>
2022-05-28 00:47:12 -0400

    publisher

---
+++ publisher/index.js
@@ -0,0 +1,27 @@
+const mqtt = require('mqtt')
+ const client  = mqtt.connect('mqtt://broker')
+// const client  = mqtt.connect('mqtt://'+process.env.HOST+':'+process.env.PORT)
+ //const client  = mqtt.connect("mqtt://192.168.1.86:1883")
+
+// print process.argv
+// process.argv.forEach(function (val, index, array) {
+//     console.log(index + ': ' + val);
+// });
+
+console.log(process.argv[2])
+
+function ConnectEvent(){
+    client.publish(process.env.TOPIC, process.argv[2])
+    client.end()
+}
+
+function MessageEvent(){
+
+    console.log(message.toString())
+    client.end()
+}
+
+
+client.on('connect', ConnectEvent)
+client.on('message',MessageEvent)
+
