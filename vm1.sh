131f0d576f2cb1013f79b8f8af6fa7e1d5155913
Andrea Sarmiento <anbesamo@gmail.com>
2022-05-28 01:07:14 -0400

    publisher part3

--- vm1.sh
+++ vm1.sh
@@ -6,7 +6,7 @@ docker network create mis-libros-net
 
 
 # Broker
-docker run -it --rm -p 1883:1883 --network mis-libros-net --name broker -v $PWD/broker/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
+docker run -it --rm -p 1883:1883 -d --network mis-libros-net --name broker -v $PWD/broker/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
 
 # docker run -it --rm -p 21062:1883 -v $PWD/broker/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
 
@@ -19,11 +19,11 @@ docker build . -t image-publisher:1.0.0
 VAR1="libros"
 # for i in {"${VAR1}/fantasia","${VAR1}/romance","${VAR1}/suspenso","${VAR1}/terror", "${VAR1}/comedia"}
 # do
-docker run -e PORT=1883 --rm -e TOPIC="${VAR1}/fantasia" -e HOST='broker' --network mis-libros-net --name pub_fantasia image-publisher:1.0.0 "Acotar"
-docker run -e PORT=1883 --rm -e TOPIC="${VAR1}/romance" -e HOST='broker' --network mis-libros-net --name pub_romance image-publisher:1.0.0 "Boulevard"
-docker run -e PORT=1883 --rm -e TOPIC="${VAR1}/suspenso" -e HOST='broker' --network mis-libros-net --name pub_suspenso image-publisher:1.0.0 "El Profesor"
-docker run -e PORT=1883 --rm -e TOPIC="${VAR1}/terror" -e HOST='broker' --network mis-libros-net --name pub_terror image-publisher:1.0.0 "El Resplandor"
-docker run -e PORT=1883 --rm -e TOPIC="${VAR1}/comedia" -e HOST='broker' --network mis-libros-net --name pub_comedia image-publisher:1.0.0 "Heartstopper"
+docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/fantasia" -e HOST='broker' --network mis-libros-net --name pub_fantasia image-publisher:1.0.0 "Acotar"
+docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/romance" -e HOST='broker' --network mis-libros-net --name pub_romance image-publisher:1.0.0 "Boulevard"
+docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/suspenso" -e HOST='broker' --network mis-libros-net --name pub_suspenso image-publisher:1.0.0 "El Profesor"
+docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/terror" -e HOST='broker' --network mis-libros-net --name pub_terror image-publisher:1.0.0 "El Resplandor"
+docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/comedia" -e HOST='broker' --network mis-libros-net --name pub_comedia image-publisher:1.0.0 "Heartstopper"
 
 # echo $i
 # done
