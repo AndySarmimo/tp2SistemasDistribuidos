8011c7267bef4ff54e1de536810c71f038281745
Andrea Sarmiento <anbesamo@gmail.com>
2022-05-29 19:50:06 -0400

    json v3

--- publisher/index.js
+++ publisher/index.js
@@ -8,27 +8,36 @@ console.log(process.argv[2])
 
 function ConnectEvent(){
     
-    var ipAdd ;
 
-    const os = require('os');
-    console.log("os hostname", os.hostname() );
+    setInterval(
+        function(){
+            var ipAdd ;
 
-
-    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-        console.log('addr: ' + add);
-        ipAdd = add
-    })
-      
+            const os = require('os');
+            console.log("os hostname", os.hostname() );
+        
+        
+            require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+                console.log('addr: ' + add);
+                ipAdd = add
+            })
+              
+            
+            var jsonFile = JSON.stringify(
+                {time: new Date().toISOString(),
+                container: os.hostname,
+                ip: ipAdd
+                });
+        
+            console.log(JSON.parse(jsonFile))
+        
+            client.publish(process.env.TOPIC, process.argv[2])
     
-    var jsonFile = JSON.stringify(
-        {time: new Date().toISOString(),
-        container: os.hostname,
-        ip: ipAdd
-        });
+    
+    },3000);
 
-    console.log(JSON.parse(jsonFile))
 
-    client.publish(process.env.TOPIC, process.argv[2])
+    
     
     // setInterval(() =&gt;  console.log("Message published"), 3000)); 
 
