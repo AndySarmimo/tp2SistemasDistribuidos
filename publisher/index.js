1d3b66a2d7dc52fee1a84d1e0234751a40f27d03
Andrea Sarmiento <anbesamo@gmail.com>
2022-05-29 19:40:59 -0400

    json v1

--- publisher/index.js
+++ publisher/index.js
@@ -3,22 +3,42 @@ const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://'+process.env.HOST+':'+process.env.PORT)
  //const client  = mqtt.connect("mqtt://192.168.1.86:1883")
 
-// print process.argv
-// process.argv.forEach(function (val, index, array) {
-//     console.log(index + ': ' + val);
-// });
 
 console.log(process.argv[2])
 
 function ConnectEvent(){
+    
+    var ipAdd ;
+
+    const os = require('os');
+    console.log("os hostname", os.hostname() );
+
+
+    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+        console.log('addr: ' + add);
+        ipAdd = add
+    })
+      
+    
+    var jsonFile = JSON.stringify(
+        {time: new Date().toISOString(),
+        container: os.hostname,
+        ip: ipAdd
+        });
+
+
     client.publish(process.env.TOPIC, process.argv[2])
-    client.end()
+    
+    // setInterval(() =&gt;  console.log("Message published"), 3000)); 
+
+
+    //client.end()
 }
 
 function MessageEvent(){
 
     console.log(message.toString())
-    client.end()
+    //client.end()
 }
 
 
--- vm1.sh
+++ vm1.sh
@@ -17,13 +17,11 @@
 cd publisher/
 docker build . -t image-publisher:1.0.0
 VAR1="libros"
-# for i in {"${VAR1}/fantasia","${VAR1}/romance","${VAR1}/suspenso","${VAR1}/terror", "${VAR1}/comedia"}
-# do
+
 docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/fantasia" -e HOST='broker' --network mis-libros-net --name pub_fantasia image-publisher:1.0.0 "Acotar"
 docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/romance" -e HOST='broker' --network mis-libros-net --name pub_romance image-publisher:1.0.0 "Boulevard"
 docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/suspenso" -e HOST='broker' --network mis-libros-net --name pub_suspenso image-publisher:1.0.0 "El Profesor"
 docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/terror" -e HOST='broker' --network mis-libros-net --name pub_terror image-publisher:1.0.0 "El Resplandor"
 docker run -e PORT=1883 --rm -d -e TOPIC="${VAR1}/comedia" -e HOST='broker' --network mis-libros-net --name pub_comedia image-publisher:1.0.0 "Heartstopper"
 
-# echo $i
-# done
+
